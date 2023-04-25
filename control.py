2ecfce48afe69f424170385dd6f2ff5b53affd17
Sergi Garcia Cantón <sgarciac01@hotmail.com>
2023-04-25 13:45:37 +0200

    VNF management added

--- control.py
+++ control.py
@@ -11,12 +11,32 @@ import ctypes
 stop = False
 connections = []
 valid_ids = [1, 2, 3]
+vnf_list = []
 
 logger = logging.getLogger('')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.FileHandler('logs/control.log', mode='w', encoding='utf-8'))
 logger.addHandler(logging.StreamHandler(sys.stdout))
 logging.getLogger('pika').setLevel(logging.WARNING)
+
+
+class VNF:
+    def __init__(self, source, target, ram, gpu, rtt, bw, previous_node, current_node, fec_linked, user_id):
+        self.source = source
+        self.target = target
+        self.ram = ram
+        self.gpu = gpu
+        self.rtt = rtt
+        self.bw = bw
+        self.previous_node = previous_node
+        self.current_node = current_node
+        self.fec_linked = fec_linked
+        self.user_id = user_id
+
+    def __str__(self):
+        return f"Source/Target: {self.source}/{self.target} | GPU: {self.gpu} cores | RAM: {self.ram} GB | " \
+               f"BW: {self.bw} kbps | RTT: {self.rtt} ms | " \
+               f"Nodes (previous/current): {self.previous_node}/{self.current_node} | Linked to FEC: {self.fec_linked}"
 
 
 class Connection:
@@ -32,9 +52,9 @@ class Connection:
         self.connected_users = connected_users
 
     def __str__(self):
-        return "FEC ID: " + self.fec_id + " | Socket ID: " + self.sock + " | MAC: " + self.mac + " | IP: " + self.ip + \
-            " | GPU: " + self.gpu + " cores | RAM: " + self.ram + " GB | BW: " + self.bw + " kbps" \
-                                                                                           " | RTT: " + self.rtt + " ms"
+        return f"FEC ID: {self.fec_id} | Socket ID: {self.sock} | MAC: {self.mac} | IP: {self.ip} | " \
+               f"GPU: {self.gpu} cores | RAM: {self.ram} GB | BW: {self.bw} kbps | RTT: {self.rtt} ms | " \
+               f"Connected IDs: {self.connected_users}"
 
 
 def publish(key, message):
@@ -50,12 +70,22 @@ def publish(key, message):
         logger.debug("[D] Published message. Key: " + key + ". Message: " + message)
     except KeyboardInterrupt:
         pass
+    except Exception as e:
+        logger.exception(e)
     finally:
         rabbit_conn.close()
 
 
-def serve_client(conn, ip, fec_id):
-    global listen_changes_thread
+def serve_client(conn, ip):
+    global listen_fec_changes_thread
+    global vnf_list
+    fec_id = 0
+    if ip == '147.83.118.154':
+        fec_id = 1
+    elif ip == '147.83.118.82':
+        fec_id = 2
+    else:
+        logger.critical('[!] Unidentifiable FEC connected!')
     connections.append(Connection(fec_id,
                                   conn,
                                   subprocess.check_output(['arp', '-n', ip]).decode().split('\n')[
@@ -108,12 +138,37 @@ def serve_client(conn, ip, fec_id):
                     connections[i].connected_users = json_data['data']['connected_users']
                     conn.send(json.dumps(dict(res=200)).encode())  # Success
                     notify_fec_state_changes()
+            elif json_data['type'] == 'vnf':
+                i = 0
+                while i < len(connections):
+                    if conn == connections[i].sock:
+                        break
+                    else:
+                        i += 1
+                if i == len(connections):
+                    conn.send(json.dumps(dict(res=404)).encode())  # Connection not found
+                else:
+                    j = 0
+                    while j < len(vnf_list):
+                        print(vnf_list[j])
+                        if vnf_list[j]['user_id'] == json_data['data']['user_id']:
+                            break
+                        else:
+                            j += 1
+                    if j == len(vnf_list):
+                        vnf_list.append(json_data['data'])
+                    elif json_data['data']['target'] == json_data['data']['current_node']:
+                        vnf_list.pop(j)
+                    else:
+                        vnf_list[j] = json_data['data']
+                    conn.send(json.dumps(dict(res=200)).encode())  # Success
+                    notify_vnf_changes()
         except TypeError:
             conn.send(json.dumps(dict(res=404)).encode())  # Error
         except json.decoder.JSONDecodeError:
             conn.send(json.dumps(dict(res=404)).encode())  # Error
-        except ConnectionResetError:
-            pass
+        except Exception as e:
+            logger.exception(e)
 
     found = False
     i = 0
@@ -129,18 +184,38 @@ def serve_client(conn, ip, fec_id):
     notify_fec_state_changes()
 
 
+def notify_vnf_changes():
+    global listen_vnf_changes_thread
+    global vnf_list
+    publish('vnf', json.dumps(vnf_list))
+    if listen_vnf_changes_thread.ident is not None:
+        kill_thread(listen_vnf_changes_thread.ident)
+    listen_vnf_changes_thread = threading.Thread(target=listen_vnf_changes)
+    listen_vnf_changes_thread.daemon = True
+    listen_vnf_changes_thread.start()
+
+
 def notify_fec_state_changes():
-    global listen_changes_thread
+    global listen_fec_changes_thread
     fec_list = []
     for connection in connections:
         fec_list.append(dict(fec_id=connection.fec_id, gpu=connection.gpu, ram=connection.ram,
                              bw=connection.bw, rtt=connection.rtt, connected_users=connection.connected_users))
     publish('fec', json.dumps(fec_list))
-    if listen_changes_thread.ident is not None:
-        kill_thread(listen_changes_thread.ident)
-    listen_changes_thread = threading.Thread(target=listen_fec_changes)
-    listen_changes_thread.daemon = True
-    listen_changes_thread.start()
+    if listen_fec_changes_thread.ident is not None:
+        kill_thread(listen_fec_changes_thread.ident)
+    listen_fec_changes_thread = threading.Thread(target=listen_fec_changes)
+    listen_fec_changes_thread.daemon = True
+    listen_fec_changes_thread.start()
+
+
+def listen_vnf_changes():
+    global vnf_list
+    previous_state = vnf_list
+    while previous_state == vnf_list:
+        time.sleep(5)
+        if previous_state == vnf_list:
+            publish('vnf', json.dumps(vnf_list))
 
 
 def listen_fec_changes():
@@ -165,7 +240,8 @@ def kill_thread(thread_id):
     logger.debug('[D] Successfully killed thread ' + str(thread_id))
 
 
-listen_changes_thread = threading.Thread(target=listen_fec_changes)
+listen_fec_changes_thread = threading.Thread(target=listen_fec_changes)
+listen_vnf_changes_thread = threading.Thread(target=listen_vnf_changes)
 
 
 def main():
@@ -182,22 +258,24 @@ def main():
         # Configure how many client the server can listen simultaneously
         server_socket.listen(1)
 
-        current_id = 0
         logger.info('[I] Control server started')
 
         # Infinite loop listening for new connections
         while True:
             conn, address = server_socket.accept()  # Accept new connection
             logger.info("[I] New connection from: " + str(address))
-            socket_thread = threading.Thread(target=serve_client, args=(conn, address[0], current_id))
+            socket_thread = threading.Thread(target=serve_client, args=(conn, address[0]))
             socket_thread.daemon = True
             socket_thread.start()
-            current_id += 1
     except KeyboardInterrupt:
         logger.info('[!] Stopping Control server...')
         stop = True
         for connection in connections:
             connection.sock.close()
+    except OSError:
+        logger.critical('[!] Error when binding address and port for server! Stopping...')
+    except Exception as e:
+        logger.exception(e)
 
 
 if __name__ == '__main__':
